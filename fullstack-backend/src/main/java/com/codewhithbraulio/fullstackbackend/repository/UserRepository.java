e276956956c4994ef6f605a36beee41296af7ba0
Braulio Ernesto Rojas Soto <brau.rojas12@gmail.com>
2023-05-19 15:02:06 -0400

    first commit

---
+++ fullstack-backend/src/main/java/com/codewhithbraulio/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codewhithbraulio.fullstackbackend.repository;
+
+import com.codewhithbraulio.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
